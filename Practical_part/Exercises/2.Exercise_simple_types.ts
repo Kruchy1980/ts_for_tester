228e27c119ba8d69b733788079442f66fc6f4718
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-03-19 16:52:00 +0100

    The exercise for simple types finished

--- Practical_part/Exercises/2.Exercise_simple_types.ts
+++ Practical_part/Exercises/2.Exercise_simple_types.ts
@@ -1,1 +1,43 @@
 // EXERCISE NO 1 -  SIMPLE TYPES
+// your task:
+// 1. Declare 4 variables (assign values only to arrays!):
+// - userName with type string
+// - userId with type number
+// - userNames with type array of strings
+// - userIds  with type array of numbers
+// 2. Assign value "Bob" to userName
+// 3. Assign value 412 to userId
+// 4. Push value of userName to userNames
+// 5. Push value of userId to userIds
+// 6. Print variables "userNames" and "userIds" to console
+
+// ❓ Questions:
+// ❓ What will happen if You don't assign value to arrays?
+
+// to test your solution in terminal you can run following command:
+// npm run ex1-1
+
+//// TODO:
+//// -----------------------HERE PLACE YOUR SOLUTION:-------------------------
+// Declaration values only
+let userName: string;
+let userId: number;
+// The complex variables - the values must be assigned even thou they are empty arrays
+let userNames: string[] = [];
+let userIds: number[] = [];
+// Assign values to  simple types variables
+userName = 'Bob';
+userId = 412;
+// Push the values to proper tables
+userNames.push(userName);
+userIds.push(userId);
+// Display the result
+console.log(userNames);
+console.log(userIds);
+//// -----------------------DON'T MODIFY CODE BELOW!-------------------------
+// Here you will find expected result of exercise
+
+// Expected output:
+// On console 👀 you should see:
+// [ 'Bob' ]
+// [ 412 ]
