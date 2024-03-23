c9ee9f0926d9c5ec1101a2a2976bf023086ba82a
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-03-23 18:04:28 +0100

    The corrections done in the code

--- Practical_part/Theory/14.Theory_modules.ts
+++ Practical_part/Theory/14.Theory_modules.ts
@@ -1,16 +1,16 @@
-// make sure, that this object has 'export' in front!
-// we can import multiple elements at once:
-import {
-  printArrayOnConsoleWithFor,
-  printArrayOnConsoleWithForeach,
-} from './helpers/sample.module';
-// Imports in TypeScript works the same way as in JavaScript.
-// Lets quickly revise this topic...
-// to import data from another module use import:
-import { sampleArray } from './helpers/sample.module';
-
-// THAT PART IS DEDICATED TO OWN MODULES WHICH CAN BE IMPORTED INTO OUR CODE - "theory_13"
 console.log("\n--------Modules and imports--------\n")
+
+// Imports in TypeScript works the same way as in JavaScript.
+
+// Lets quickly revise this topic...
+
+// to import data from another module use import:
+import { sampleArray } from "./helpers/sample.module"
+
+// make sure, that this object has 'export' in front!
+
+// we can import multiple elements at once:
+import { printArrayOnConsoleWithFor, printArrayOnConsoleWithForeach } from "./helpers/sample.module"
 
 // Good practice: we put all imports at the top of the file
 
