a608f99da75a94346650c276f9b9ce8ab5169ad3
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-03-23 14:12:32 +0100

    The part of the classes works exercise

---
+++ Description/Info_about_classes.md
@@ -0,0 +1,7 @@
+##  THIS FILE CONTAINS SOME USEFUL LINKS ABOUT CLASSES AND CONSTRUCTORS AS WELL AS OBJECTS IN CLASSES
+
+### Useful links:
+1. **Intro to Classes** - *Only for the Course* - ***https://jaktestowac.pl/lesson/api6s03l07/***
+2. **Classes and constructors** - *Only for the Course* - ***https://jaktestowac.pl/lesson/api6s03l07/***
+3. **TS - Classes** - ***https://www.typescriptlang.org/docs/handbook/2/classes.html#handbook-content***
+4. **TS - Object types** - ***https://www.typescriptlang.org/docs/handbook/2/classes.html#handbook-content***
--- Practical_part/Exercises/6.Exercise_classes.ts
+++ Practical_part/Exercises/6.Exercise_classes.ts
@@ -1,1 +1,88 @@
 // THE EXERCISE ABOUT CLASS-OBJECT-MODEL "exercise_5"
+// your task:
+// 1. Declare class with following details:
+// - name: TransactionData
+// - with property id of type string
+// - with property version of type number
+// - with constructor:
+// --- that needs one parameter to set value of property id
+// --- and sets version to 0
+// - with one method:
+// --- name: doOperation()
+// --- prints information about property version value
+// --- method increase value of property version by 1
+// --- returns value of version
+// 2. Create object from class TransactionData
+// 3. Invoke method doOperation() 4 times
+// 4. Print return value of last invoked doOperation()
+
+// TIP: Remember about types!
+
+// to test your solution in terminal you can run following command:
+// npm run ex2-2
+
+//// TODO:
+//// -----------------------HERE PLACE YOUR SOLUTION:-------------------------
+// 1. Declare class with following details:
+// - name: TransactionData
+// - with property id of type string
+// - with property version of type number
+// - with constructor:
+// --- that needs one parameter to set value of property id
+// --- and sets version to 0
+// - with one method:
+// --- name: doOperation()
+// --- prints information about property version value
+// --- method increase value of property version by 1
+// --- returns value of version
+class TransactionData {
+    // Class values - access modificators
+    // private - visible only for the specific class
+    // public - visible everywhere
+    // protected - no possible to be changed
+    private id: string;
+    public version: number;
+    // Constructor
+    constructor(id: string, version: number) {
+        this.id = id
+        this.version = 0;
+    }
+    // Class method
+    // doOperation = (): string | number => {
+    //     // console.log(`Last value of version: ${this.version}`);
+    //     this.version++;
+    //     return `Last value of version: ${this.version}`;
+    // }
+    // Class method
+    public doOperation(): void {
+        console.log(`Last value of version: ${this.version}`);
+        this.version++;
+        // return `Last value of version: ${this.version}`;
+    }
+}
+// 2. Create object from class TransactionData
+const trans_1 = new TransactionData('1', 0);
+
+// 3. Invoke method doOperation() 4 times
+for (let i = 0; i < 4; i++) {
+    trans_1.doOperation();
+};
+// console.log(trans_1.doOperation());
+// console.log(trans_1.doOperation());
+// console.log(trans_1.doOperation());
+// console.log(trans_1.doOperation());
+// trans_1.doOperation();
+// trans_1.doOperation();
+// trans_1.doOperation();
+// trans_1.doOperation();
+
+//// -----------------------DON'T MODIFY CODE BELOW!-------------------------
+// Here you will find expected result of exercise
+
+// Expected output:
+// On console 👀 you should see:
+// Last value of version: 0
+// Last value of version: 1
+// Last value of version: 2
+// Last value of version: 3
+// 4
