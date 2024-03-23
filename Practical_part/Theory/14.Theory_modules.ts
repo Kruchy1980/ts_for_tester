e89476606fcdc9d968e99aef8f62cbbbd268355c
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-03-23 17:58:06 +0100

    The initial of theory used for sample modules customized by user

---
+++ Practical_part/Theory/14.Theory_modules.ts
@@ -0,0 +1,34 @@
+// make sure, that this object has 'export' in front!
+// we can import multiple elements at once:
+import {
+  printArrayOnConsoleWithFor,
+  printArrayOnConsoleWithForeach,
+} from './helpers/sample.module';
+// Imports in TypeScript works the same way as in JavaScript.
+// Lets quickly revise this topic...
+// to import data from another module use import:
+import { sampleArray } from './helpers/sample.module';
+
+// THAT PART IS DEDICATED TO OWN MODULES WHICH CAN BE IMPORTED INTO OUR CODE - "theory_13"
+console.log("\n--------Modules and imports--------\n")
+
+// Good practice: we put all imports at the top of the file
+
+console.log("imported sampleArray:", sampleArray)
+// on console 👀 you will get:
+// imported sampleArray: [ 'Python', 'Java', 'JavaScript', 'C#' ]
+
+// after importing functions, we can use it as before:
+printArrayOnConsoleWithFor(sampleArray)
+// on console 👀 you will get:
+// printArrayOnConsoleWithFor: Python
+// printArrayOnConsoleWithFor: Java
+// printArrayOnConsoleWithFor: JavaScript
+// printArrayOnConsoleWithFor: C#
+
+printArrayOnConsoleWithForeach(sampleArray)
+// on console 👀 you will get:
+// printArrayOnConsoleWithForeach: Python
+// printArrayOnConsoleWithForeach: Java
+// printArrayOnConsoleWithForeach: JavaScript
+// printArrayOnConsoleWithForeach: C#
