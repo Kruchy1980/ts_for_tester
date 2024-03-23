dc85450139a7d2fe8215dbf100d62444e2824d69
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-03-23 12:39:40 +0100

    The part of the Interface exercises added to the repo

--- Practical_part/Exercises/5.Exercise_interfase.ts
+++ Practical_part/Exercises/5.Exercise_interfase.ts
@@ -22,7 +22,7 @@
 interface UserData {
     id: number;
     fullName: string;
-    pass: string;
+    pass: string | number;
     age: number;
     optional?: string;
 }
@@ -41,7 +41,27 @@ const user_2: UserData = {
     pass: 'Ol!gArc#y',
     age: 24,
 }
-// TODO tomorrow finish the task 
+// 3_A. Display on console both objects - I. Solution 
+// console.log(user_1);
+// console.log(user_2);
+// 3_B. Display on console both objects - II. Solution 
+// Prepare function for printing the data:
+const printUserData = (id: number, name: string, password: string | number, age: number, text = 'No Data' ): void => {
+    console.log(`id: ${id} \n name: ${name} \n pass: ${password} \n age: ${age} \n text: ${text} \n`)
+}; 
+// Use function for displaying data for both interfaces
+printUserData(user_1.id, user_1.fullName, user_1.pass, user_1.age, user_1.optional);
+printUserData(user_2.id, user_2.fullName, user_2.pass, user_2.age);
+
+// 4. Change value of id in both objects:
+// - change id from first object with value from second object
+// - change id from second object with value from first object
+const changedId = user_1.id;
+user_1.id = user_2.id;
+user_2.id = changedId;
+// 4. Display on console both objects
+printUserData(user_1.id, user_1.fullName, user_1.pass, user_1.age, user_1.optional);
+printUserData(user_2.id, user_2.fullName, user_2.pass, user_2.age);
 
 //// -----------------------DON'T MODIFY CODE BELOW!-------------------------
 // Here you will find expected result of exercise
