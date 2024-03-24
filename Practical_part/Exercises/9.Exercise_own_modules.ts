e75eed699212c50cb7bafecd8b6d0f4c3a766e24
Kruchy1980 <jakub.kruszynski@vp.pl>
2024-03-24 15:23:07 +0100

    The part of the own modules added to the project

---
+++ Practical_part/Exercises/9.Exercise_own_modules.ts
@@ -0,0 +1,30 @@
+// THAT PART IS DEDICAGED TO OWN MODULES IN TS - "exercise_8"
+// your task:
+//
+// 1. Create new module "data.helpers" in directory /helpers
+// 2. Prepare a function with name getBiggestNumber() that:
+// -- will receive one parameter "values" that is a array of numbers
+// -- will return a number
+// -- inside function check values in given list and will return largest number
+// 3. Use that function in this script:
+// -- pass a list of values and check if function returns largest number
+import {
+  getBiggestNumber,
+  largest,
+} from './helpers/data.helpers';
+
+// to test your solution in terminal you can run following command:
+// npm run ex3-1
+
+const values = [8, 4, 3, 4, 2, -2, 2]
+let largestValue: number;
+//// TODO:
+//// -----------------------HERE PLACE YOUR SOLUTION:-------------------------
+
+// Assigning max value to largestValue
+largestValue = getBiggestNumber(values, largest);
+
+//// -----------------------DON'T MODIFY CODE BELOW!-------------------------
+// Here you will find expected result of exercise
+
+console.log("Largest value of ", values, " is ", largestValue)
---
+++ Practical_part/Exercises/helpers/data.helpers.ts
@@ -0,0 +1,18 @@
+//  IN THIS FILE THE OWN CREATED MODULE IS ADDED
+// 1. Create new module "data.helpers" in directory /helpers
+// 2. Prepare a function with name getBiggestNumber() that:
+// -- will receive one parameter "values" that is a array of numbers
+// -- will return a number
+// -- inside function check values in given list and will return largest number
+// const values = [8, 4, 38, 4, 2, -2, 2];
+export let largest = 0; 
+export const getBiggestNumber = (data: number[], maxNum: number): number => {
+    maxNum = 0;
+    for (let i=0; i < data.length; i++) {
+        if (data[i] > maxNum) {
+            maxNum = data[i];
+        };
+    };
+    return maxNum;
+}
+// console.log(getBiggestNumber(values, largest));
